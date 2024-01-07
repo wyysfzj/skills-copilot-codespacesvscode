806a77c6c24941951b02dea0dcffb91f3568c904
wyysfzj <wyysf.zj@gmail.com>
2024-01-07 02:37:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    member.innerHTML = "Member";
+    member.style.color = "red";
+    member.style.fontWeight = "bold";
+    member.style.fontSize = "30px";
+    member.style.textAlign = "center";
+    member.style.marginTop = "20px";
+    member.style.marginBottom = "20px";
+    member.style.marginLeft = "auto";
+    member.style.marginRight = "auto";
+    member.style.width = "50%";
+    member.style.border = "2px solid red";
+    member.style.padding = "20px";
+    member.style.borderRadius = "10px";
+    member.style.boxShadow = "0 0 10px red";
+
+}
